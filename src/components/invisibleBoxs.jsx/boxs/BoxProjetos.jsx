063723a51cbe39c53fc65db637589779a941a55d
Mateus Reis <93735156+MateusRSilva@users.noreply.github.com>
2024-07-31 10:21:55 -0300

    second-commit

--- src/components/invisibleBoxs.jsx/boxs/BoxProjetos.jsx
+++ src/components/invisibleBoxs.jsx/boxs/BoxProjetos.jsx
@@ -4,8 +4,15 @@ import InvisibleContext from "../../../context/InvisibleBoxContext";
 const BlockProjetos = () => {
     const { setSelectedObject, setSelectedTopic } = useContext(InvisibleContext);
 
+    const handleClick = () => {
+        setTimeout(() => {
+            setSelectedObject('Projetos');
+            setSelectedTopic('Projetos');
+        }, 0); // Delay de 500ms, ajuste conforme necessário
+    };
+
     return (
-        <mesh position={[0.6, 0.2, -0.4]} onClick={() => { setSelectedObject('Projetos'); setSelectedTopic('Projetos'); }}>
+        <mesh name="Projetos" position={[0.6, 0.2, -0.4]} onClick={handleClick}>
             <boxGeometry args={[1.3, 1.3, 1.3]} />
             <meshStandardMaterial visible={false} />
         </mesh>
